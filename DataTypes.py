0a003e664a573db3af584ec11132b8f90b7cf992
MirAtharHussain <miratharhussain52@gmail.com>
2021-03-11 22:44:02 +0530

    Operations on DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,16 @@
+#int DataTpe: To represent integer's.
+a=10
+print(type(a))
+print(a)
+
+#float DataType: To represent Floating point data.
+
+f=1.234
+type(f)
+print(f)
+
+#complex DataType: a and b contain Intergers OR Floating Point Values, contians Real and Imaginary part.
+a=10+1.5j
+b=20+2.5j
+c = a+b
+print(c)
